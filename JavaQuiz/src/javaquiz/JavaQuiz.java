6a7ab06f203c42f7b233c49fd3ead176a8efcb64
atiyahzahra <106978188+atiyahzahra@users.noreply.github.com>
2023-01-07 21:03:45 +0700

    Add files via upload

---
+++ JavaQuiz/src/javaquiz/JavaQuiz.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaquiz;
+
+/**
+ *
+ * @author ATIYAH ZAHRA
+ */
+public class JavaQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // this is the main class and project start from this class
+        new Start().setVisible(true);
+        
+    }
+    
+}
